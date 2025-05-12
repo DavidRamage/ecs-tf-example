a12e71e95aa14166a03f83f293a7e9d7aa681400
David Ramage <david.p.ramage@gmail.com>
2025-05-11 17:58:34 -0700

    initial commit of cluster module

---
+++ cluster/cluster.tf
@@ -0,0 +1,17 @@
+resource "aws_kms_key" "cluster-key" {
+  description = "ecs_cluster_key"
+  deletion_window_in_days = 7
+}
+
+resource "aws_cloudwatch_log_group" "cluster-logs" {
+  name = "cluster-logs"
+}
+
+resource "aws_ecs_cluster" "demo-cluster" {
+  name = "demo-cluter"
+  configuration {
+    execute_command_configuration {
+      kms_key_id = aws_kms_key.cluster-key.arn
+    }
+  }
+}
